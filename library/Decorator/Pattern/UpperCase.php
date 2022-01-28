jpauli/PHP-Decorator-Pattern

First implementation

library/Decorator/Pattern/UpperCase.php
@@ -1,0 +1,63 @@
+<?php
+/**
+* PHP-Decorator-Pattern
+*
+* Copyright (c) 2011, Julien Pauli <jpauli@php.net>.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in
+* the documentation and/or other materials provided with the
+* distribution.
+*
+* * Neither the name of Julien Pauli nor the names of his
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+* @author Julien Pauli <jpauli@php.net>
+* @copyright 2011 Julien Pauli <jpauli@php.net>
+* @license http://www.opensource.org/licenses/bsd-license.php BSD License
+*/
+
+namespace Decorator\Pattern;
+
+/**
+* Uppercase Decorator
+*
+* @author Julien Pauli <jpauli@php.net>
+* @copyright 2011 Julien Pauli <jpauli@php.net>
+* @license http://www.opensource.org/licenses/bsd-license.php BSD License
+* @version Release: @package_version@
+*/
+class UpperCase extends Decorator
+{
+    /**
+     * @param string $what
+     * @return string
+     * @see Decorator\Pattern\Writer::write()
+     */
+    public function write(string $what)
+    {
+        return strtoupper($this->decorator->write($what));
+    }
+}

tests/example.php
@@ -1,0 +1,9 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$stylo = new Decorator\Pattern\UpperCase(
+          new Decorator\Pattern\Html\Bold(
+            new Decorator\Pattern\Html\Italic(
+              new Decorator\Pen())));
+
+echo $stylo->write("hello"); /* <B><I>HELLO</I></B> */

jpauli/PHP-Pattern-Decorator

library/Decorator/Pattern/UpperCase.php
@@ -1,0 +1,58 @@
+<?php
+/**
+* PHP-Decorator-Pattern
+*
+* Copyright (c) 2011, Julien Pauli <jpauli@php.net>.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in
+* the documentation and/or other materials provided with the
+* distribution.
+*
+* * Neither the name of Julien Pauli nor the names of his
+* contributors may be used to endorse or promote products derived
+* from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+* @author Julien Pauli <jpauli@php.net>
+* @copyright 2011 Julien Pauli <jpauli@php.net>
+* @license http://www.opensource.org/licenses/bsd-license.php BSD License
+*/
+
+namespace Decorator\Pattern;
+
+/**
+* Uppercase Decorator
+*
+* @author Julien Pauli <jpauli@php.net>
+* @copyright 2011 Julien Pauli <jpauli@php.net>
+* @license http://www.opensource.org/licenses/bsd-license.php BSD License
+* @version Release: @package_version@
+*/
+class UpperCase extends Decorator
+{
+    public function write(string $what) : string
+    {
+        return strtoupper($this->_decorator->write($what));
+    }
+}

tests/example.php
@@ -1,0 +1,9 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$pen = new Decorator\Pattern\UpperCase(
+          new Decorator\Pattern\Html\Bold(
+            new Decorator\Pattern\Html\Italic(
+              new Decorator\Pen())));
+
+echo $pen->write("hello"); /* <B><I>HELLO</I></B> */